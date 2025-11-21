88d3443901090ea993bba4c4a86b7cfaab0bbc0b
EvaTP <evathpr@icloud.com>
2025-11-21 20:06:00 +0100

    fix(about): responsive mobile

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Fondu au Noir",
+        short_name: "FonduNoir",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+
   resolve: {
     alias: {
       "@": resolve(__dirname, "src"),
